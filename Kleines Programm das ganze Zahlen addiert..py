4fb23c922a0306475aff1a15e77e489b8b4a0dba
Sebastian Groß <Gross.Sn@gmx.de>
2017-03-30 12:26:19 +0200

    Add files via upload

---
+++ Kleines Programm das ganze Zahlen addiert..py
@@ -0,0 +1,14 @@
+
+y = int(input("Wählen Sie eine ganze Zahl: "))
+x = int(input("Wählen Sie eine weitere ganze Zahl: "))
+
+
+Ergebnis = x+y
+
+if Ergebnis > 0:
+    print("Die Summe ist größer Null: ", Ergebnis)
+
+elif Ergebnis <= 0:
+    print("Die Summe ist kleiner/gleich Null: ", Ergebnis)
+            
+   
